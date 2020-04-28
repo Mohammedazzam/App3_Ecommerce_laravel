030366448cf2b9a260830c07e4ab4b27274354f3
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-28 15:47:29 +0300

    9(2 Project Brands CRUD Part 2كود رفع الصورة في البراند )

--- app/Http/Controllers/Admin/Category/BrandController.php
+++ app/Http/Controllers/Admin/Category/BrandController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\Category;
 use App\Http\Controllers\Controller;
 use App\Model\Admin\Brand;
 use Illuminate\Http\Request;
+use DB;
 
 class BrandController extends Controller
 {
@@ -17,7 +18,40 @@ class BrandController extends Controller
         $brand = Brand::all();
         return view('admin.category.brand',compact('brand'));
     }
-    public function storebrand(){
+
+
+    public function storebrand(Request $request){
+        $validateData = $request->validate([
+            'brand_name' => 'required|unique:brands|max:55',
+
+        ]);
+
+        $data = array();
+        $data['brand_name'] = $request->brand_name;
+        $image = $request->file('brand_logo');
+        if ($image) {
+            $image_name = date('dmy_H_s_i');
+            $ext = strtolower($image->getClientOriginalExtension());
+            $image_full_name = $image_name.'.'.$ext;
+            $upload_path = 'public/media/brand/';//راح ينشئ مجلد في البابلك باسم media وداخله مجلد باسم brand ويحفظ فيه الصورة
+            $image_url = $upload_path.$image_full_name;
+            $success = $image->move($upload_path,$image_full_name);
+
+            $data['brand_logo'] = $image_url;
+            $brand = DB::table('brands')->insert($data);
+            $notification=array(
+                'messege'=>'Brand Inserted Successfully',
+                'alert-type'=>'success'
+            );
+            return Redirect()->back()->with($notification);
+        }else{
+            $brand = DB::table('brands')->insert($data);
+            $notification=array(
+                'messege'=>'Its Done',
+                'alert-type'=>'success'
+            );
+            return Redirect()->back()->with($notification);
+        }
 
     }
 }
