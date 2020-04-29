3a54e3b0e982d47ac11b0d8977d83e3cd497df0f
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-29 03:54:10 +0300

    10(2 Ecommerce Sub category Part 2)

--- app/Http/Controllers/Admin/Category/SubCategoryController.php
+++ app/Http/Controllers/Admin/Category/SubCategoryController.php
@@ -15,16 +15,30 @@ class SubCategoryController extends Controller
 
     public function subcategories(){
         $category = DB::table('categories')->get();
-        $subcat= DB::table('subcategories')
-                    ->join('categories','subcategories.category_id','categories.id')
-                    ->select('subcategories.*','categories.category_name')
-                    ->get();
-
+        $subcat = DB::table('subcategories')
+            ->join('categories','subcategories.category_id','categories.id')
+            ->select('subcategories.*','categories.category_name')
+            ->get();
         return view('admin.category.subcategory',compact('category','subcat'));
     }
 
 
     public function storesubcat(Request $request){
 
+        $validateData = $request->validate([
+            'category_id' => 'required',
+            'subcategory_name' => 'required',
+
+        ]);
+
+        $data = array();
+        $data['category_id'] = $request->category_id;
+        $data['subcategory_name'] = $request->subcategory_name;
+        DB::table('subcategories')->insert($data);
+        $notification=array(
+            'messege'=>'Sub Category Inserted Successfully',
+            'alert-type'=>'success'
+        );
+        return Redirect()->back()->with($notification);
     }
 }
