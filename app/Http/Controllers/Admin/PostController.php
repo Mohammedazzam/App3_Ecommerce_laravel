5ff5c2e475d903de1499382cb74c8d5b61390cfe
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-05-08 03:38:43 +0300

    13(5. Add Multi language Prepare Post Category  عملية رفع البوست)

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -15,14 +15,16 @@ class PostController extends Controller
         $this->middleware('auth:admin');
     }
 
-    public function BlogCatList(){
+    public function BlogCatList()
+    {
         $blogcat = DB::table('post_category')->get();
-        return view('admin.blog.category.index',compact('blogcat'));
+        return view('admin.blog.category.index', compact('blogcat'));
 
     }
 
 
-    public function BlogCatStore(Request $request){
+    public function BlogCatStore(Request $request)
+    {
         $validateDate = $request->validate([
             'category_name_en' => 'required|max:255',
             'category_name_in' => 'required|max:255',
@@ -33,58 +35,93 @@ class PostController extends Controller
         $data['category_name_en'] = $request->category_name_en;
         $data['category_name_in'] = $request->category_name_in;
         DB::table('post_category')->insert($data);
-        $notification=array(
-            'messege'=>'Blog Category Added Successfully',
-            'alert-type'=>'success'
+        $notification = array(
+            'messege' => 'Blog Category Added Successfully',
+            'alert-type' => 'success'
         );
         return Redirect()->back()->with($notification);
     }
 
 
-
-    public function DeleteBlogCat($id){
-        DB::table('post_category')->where('id',$id)->delete();
-        $notification=array(
-            'messege'=>'Blog Category Deleted Successfully',
-            'alert-type'=>'success'
+    public function DeleteBlogCat($id)
+    {
+        DB::table('post_category')->where('id', $id)->delete();
+        $notification = array(
+            'messege' => 'Blog Category Deleted Successfully',
+            'alert-type' => 'success'
         );
         return Redirect()->back()->with($notification);
 
     }
 
 
-
-    public function EditBlogCat($id){
-        $blogcatedit = DB::table('post_category')->where('id',$id)->first();
-        return view('admin.blog.category.edit',compact('blogcatedit'));
+    public function EditBlogCat($id)
+    {
+        $blogcatedit = DB::table('post_category')->where('id', $id)->first();
+        return view('admin.blog.category.edit', compact('blogcatedit'));
 
     }
 
-    public function UpdateBlogCat(Request $request,$id){
+    public function UpdateBlogCat(Request $request, $id)
+    {
         $data = array();
         $data['category_name_en'] = $request->category_name_en;
         $data['category_name_in'] = $request->category_name_in;
-        DB::table('post_category')->where('id',$id)->update($data);
-        $notification=array(
-            'messege'=>'Blog Category Update Successfully',
-            'alert-type'=>'success'
+        DB::table('post_category')->where('id', $id)->update($data);
+        $notification = array(
+            'messege' => 'Blog Category Update Successfully',
+            'alert-type' => 'success'
         );
         return Redirect()->route('add.blog.categorylist')->with($notification);
 
     }
 
 
-    public function Create(){
+    public function Create()
+    {
 
         $blogcategory = DB::table('post_category')->get();
-        return view('admin.blog.create',compact('blogcategory'));
+        return view('admin.blog.create', compact('blogcategory'));
 
     }
 
-    public function store(){
+    public function store(Request $request)
+    {
+
+        $data = array();
+        $data['post_title_en'] = $request->post_title_en;
+        $data['post_title_in'] = $request->post_title_in;
+        $data['category_id'] = $request->category_id;
+        $data['details_en'] = $request->details_en;
+        $data['details_in'] = $request->details_in;
+        $data['post_image'] = $request->post_image;
+
+        $post_image = $request->file('post_image');
+
+        if ($post_image) {
+            $post_image_name = hexdec(uniqid()) . '.' . $post_image->getClientOriginalExtension();
+            Image::make($post_image)->resize(400, 200)->save('public/media/post/' . $post_image_name);
+            $data['post_image'] = 'public/media/post/' . $post_image_name;
+
+            DB::table('posts')->insert($data);
+            $notification = array(
+                'messege' => 'Post Inserted Successfully',
+                'alert-type' => 'success'
+            );
+            return Redirect()->back()->with($notification);
+
+
+        }else{
+            $data['post_image']='';
+            DB::table('posts')->insert($data);
+            $notification = array(
+                'messege' => 'Post Inserted Without Image',
+                'alert-type' => 'success'
+            );
+            return Redirect()->back()->with($notification);
+
+        }
+
 
     }
-
-
-
 }
